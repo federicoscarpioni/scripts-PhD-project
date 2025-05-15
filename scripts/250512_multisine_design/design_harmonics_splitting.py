0cbd879fe6b03743f9c03e8b7ad2130410868c60
= <=>
2025-05-15 12:13:21 +0200

    Added scripts for multisine desing

---
+++ scripts/250512_multisine_design/design_harmonics_splitting.py
@@ -0,0 +1,88 @@
+from multisine import Multisine, compute_crest_factor, compute_crest_factor
+import numpy as np
+import matplotlib.pyplot as plt
+
+harmonics = np.loadtxt('scripts/250512_multisine_design/data/harmonics_8dec_quasi-log-8pts_no_intermod_second.txt')
+base_frequency = 0.01
+max_frequency = 100000
+frequencies = harmonics * base_frequency
+
+# Remove higher frequencies
+frequencies = frequencies[0:np.where(frequencies>max_frequency)[0][0]]
+frequencies[-1] = max_frequency
+
+splitting_index = 28 # 100 Hz
+
+
+fig, (ax1, ax2, ax3, ax4) = plt.subplots(4, 1, figsize=(15,3),sharex=True, gridspec_kw={'hspace': 0})
+
+ax1.vlines(frequencies, 0 , 1, )
+
+# Regenerate from 100 Hz
+base_frequency = 100 
+max_frequency = 100000
+frequencies_high = harmonics * base_frequency 
+frequencies_high = frequencies_high[0:np.where(frequencies_high>max_frequency)[0][0]]
+frequencies_high[-1] = max_frequency
+
+ax2.vlines(frequencies_high, 0 , 1, color = 'orange')
+ax2.vlines(frequencies[0:splitting_index], 0 , 1)
+ax2.vlines(frequencies[splitting_index:], 0 , 1, color= 'gray', alpha = 0.3)
+
+# Regenerate from 10 Hz
+base_frequency = 10 
+max_frequency = 100000
+frequencies_high = harmonics * base_frequency 
+frequencies_high = frequencies_high[0:np.where(frequencies_high>max_frequency)[0][0]]
+frequencies_high[-1] = max_frequency
+# Discard one decade
+frequencies_high = frequencies_high[4:]
+
+ax3.vlines(frequencies_high, 0 , 1, color = 'orange')
+ax3.vlines(frequencies[0:splitting_index], 0 , 1)
+ax3.vlines(frequencies[splitting_index:], 0 , 1, color= 'gray', alpha = 0.3)
+
+# Regenerate from 1 Hz
+base_frequency = 1 
+max_frequency = 100000
+frequencies_high = harmonics * base_frequency 
+frequencies_high = frequencies_high[0:np.where(frequencies_high>max_frequency)[0][0]]
+frequencies_high[-1] = max_frequency
+# Discard the new decade
+frequencies_high = frequencies_high[12:]
+
+ax4.vlines(frequencies_high, 0 , 1, color = 'orange')
+ax4.vlines(frequencies[0:splitting_index], 0 , 1)
+ax4.vlines(frequencies[splitting_index:], 0 , 1, color= 'gray', alpha = 0.3)
+
+# Set all plots to log scale
+ax1.set_xscale('log')
+ax2.set_xscale('log')
+ax3.set_xscale('log')
+ax4.set_xscale('log')
+
+# Hide y-axis labels and ticks
+ax1.set_yticks([])
+ax1.set_yticklabels([])
+ax2.set_yticks([])
+ax2.set_yticklabels([])
+ax3.set_yticks([])
+ax3.set_yticklabels([])
+ax4.set_yticks([])
+ax4.set_yticklabels([])
+ax1.set_ylim(0, 1) 
+ax2.set_ylim(0, 1)
+ax3.set_ylim(0, 1)
+ax4.set_ylim(0, 1)
+
+# Add tites
+ax1.set_title('Comparison between single harmonic sequence and two summed sequences.')
+ax1.set_ylabel('Quasi-log harmonic sequence', labelpad=10, rotation=0, ha='right', va='center')
+ax2.set_ylabel('Restart harmonic seires from 100 Hz', labelpad=10, rotation=0, ha='right', va='center')
+ax3.set_ylabel('Restart harmonic seires from 10 Hz', labelpad=10, rotation=0, ha='right', va='center')
+ax4.set_ylabel('Restart harmonic seires from 1 Hz', labelpad=10, rotation=0, ha='right', va='center')
+
+
+
+plt.tight_layout()
+plt.show()
---
+++ scripts/250512_multisine_design/harmonic_sum_with_splitting.py
@@ -0,0 +1,78 @@
+from multisine import Multisine, compute_crest_factor, compute_crest_factor
+import numpy as np
+import matplotlib.pyplot as plt
+
+harmonics = np.loadtxt('scripts/250512_multisine_design/data/harmonics_8dec_quasi-log-8pts_no_intermod_second.txt')
+base_frequency = 0.01
+max_frequency = 100000
+frequencies = harmonics * base_frequency
+
+# Remove higher frequencies
+frequencies = frequencies[0:np.where(frequencies>max_frequency)[0][0]]
+frequencies[-1] = max_frequency
+
+# # Generating the complete multisine to perform first the phase minimization
+amplitudes = np.ones(frequencies.size)*0.005  # V
+sampling_frequency = 250000
+# ms = Multisine(
+#     sampling_frequency, 
+#     frequencies, 
+#     amplitudes,
+# )
+# ms.best_random_phases(5)
+# ms.normalize_waveform()
+# ms.plot('voltage')
+# ms.fourier_analysis(5)
+# ms.plot_dft((0.001,sampling_frequency//2))
+# ms.plot_phase((0.001,sampling_frequency//2))
+
+
+# Splitting into two waveforms
+
+splitting_index = 28 
+# phases = ms.phases
+
+sampling_frequency_low = 1000
+
+msfirst = Multisine(
+    sampling_frequency_low,
+    frequencies[0:splitting_index], 
+    amplitudes[0:splitting_index], 
+)
+msfirst.best_random_phases(200)
+msfirst.normalize_waveform()
+msfirst.plot('voltage')
+msfirst.fourier_analysis(6)
+msfirst.plot_dft((0.001, sampling_frequency_low//2))
+
+base_frequency = 1 # Regenarate from one decade below the desired
+max_frequency = 100000
+frequencies_high = harmonics * base_frequency 
+frequencies_high = frequencies_high[0:np.where(frequencies_high>max_frequency)[0][0]]
+frequencies_high[-1] = max_frequency
+# Discard the new decade
+frequencies_high = frequencies_high[12:]
+
+# Generate multisine high band
+sampling_frequency_high = 1000000
+waveform_period = 1
+number_of_points_high = int(waveform_period*sampling_frequency_high)
+mssecond = Multisine(
+    sampling_frequency_high,
+    frequencies_high, #frequencies[splitting_index:], 
+    amplitudes[splitting_index:],
+    # phases[splitting_index:],
+    number_points = number_of_points_high,
+)
+mssecond.best_random_phases(200)
+mssecond.normalize_waveform()
+mssecond.plot('voltage')
+mssecond.fourier_analysis(6)
+mssecond.plot_dft((1,sampling_frequency_high//2))
+
+plt.show()
+
+# Save waveforms
+multisine_basename = '2505151210multisine_splitted_quasi-log_100kHz-10mHz_8ptd_flat_norm_random_phases'
+msfirst.save('C:/multisine_collection/'+multisine_basename+'/low_band/')
+mssecond.save('C:/multisine_collection/'+multisine_basename+'/high_band/')
